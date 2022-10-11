bef28f974cfd89049fcc49e5c1802fc7731c2323
Adrian Rydz <37881682+arydz@users.noreply.github.com>
2022-10-11 22:25:13 +0200

    Add openapi (swagger) support (#2)

--- src/main/java/com/arydz/stockfinder/StockFinderDemoApplication.java
+++ src/main/java/com/arydz/stockfinder/StockFinderDemoApplication.java
@@ -1,9 +1,12 @@
 package com.arydz.stockfinder;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "SF Demo API", version = "1.0", description = "Stock Finder Service (Demo) Documentation API v1.0"))
 public class StockFinderDemoApplication {
 
 	public static void main(String[] args) {
